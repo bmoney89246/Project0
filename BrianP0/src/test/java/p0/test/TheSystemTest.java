1b45f0ad4caa4f8ad7f18499d4024a4ba3b8bdf9
brian <brian.money92@yahoo.com>
2019-09-30 18:26:15 -0400

    Project0  turn in

--- BrianP0/src/test/java/p0/test/TheSystemTest.java
+++ BrianP0/src/test/java/p0/test/TheSystemTest.java
@@ -1,6 +1,7 @@
 package p0.test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.After;
@@ -8,10 +9,18 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import p0.pojos.User;
+import p0.service.Login;
 import p0.service.TheSystem;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TheSystemTest {
+
+	@Mock
+	TheSystem sys = mock(TheSystem.class);
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -23,6 +32,7 @@ public class TheSystemTest {
 
 	@Before
 	public void setUp() throws Exception {
+		sys = new TheSystem();
 	}
 
 	@After
@@ -30,8 +40,17 @@ public class TheSystemTest {
 	}
 
 	@Test
-	public void loginOrRegister() {
-		when(TheSystem.LoginOrRegister());
+	public void TheSystemTest() {
+		assertTrue(sys.LoginOrRegister());
+		assertFalse(sys.LoginOrRegister());
+//		User user = new User();
+//		User user2 = new User();
+//		user2.setUsername("Bmoney");
+//		user2.setPassword("Password");
+//		//when(TheSystem.in.nextLine()).thenReturn("Bmoney");
+//		when(sys.LoginOrRegister()).thenReturn(user2);
+//		assertEquals("Should be Bmoney, Password", user.getUsername(), login.InitialLogin(user));
+//		assertEquals("Should be Bmoney, Password", user.getPassword(), login.InitialLogin(user));
 	}
 
 }
